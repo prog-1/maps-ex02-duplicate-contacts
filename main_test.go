082051c0ac42ed7bf7f72f50aa48aefd3c17bf22
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2022-01-12 18:16:35 +0200

    working and passing tests

--- main_test.go
+++ main_test.go
@@ -13,12 +13,12 @@ func TestCreatePhoneBook(t *testing.T) {
 	}{
 		{[]string{"A1", "A2", "A3"}, []string{"+371222", "+371111", "+371333"}, map[string]string{"A1": "+371222", "A2": "+371111", "A3": "+371333"}},
 		{[]string{"+371234"}, []string{"A2"}, map[string]string{"+371234": "A2"}},
-		{[]string{"A1", "A1"}, []string{"2266", "+3389"}, map[string]string{"A2": "+3389"}},
+		{[]string{"A1", "A1"}, []string{"2266", "+3389"}, map[string]string{"A1": "+3389"}},
 		{[]string{",!"}, []string{":0="}, map[string]string{",!": ":0="}},
 		{[]string{"A1", "A2", "A9", "A1", "A9"}, []string{"11", "22", "99", "88", "99"}, map[string]string{"A1": "88", "A2": "22", "A9": "99"}},
 	} {
 		got := createPhoneBook(tc.names, tc.numbers)
-		if reflect.DeepEqual(got, tc.want) {
+		if !reflect.DeepEqual(got, tc.want) {
 			t.Errorf("createPhoneBook(%v, %v) = %v, want = %v", tc.names, tc.numbers, got, tc.want)
 		} // Every Test fails, although everywhere got == want ??
 		//createPhoneBook([A1 A2 A9 A1 A9], [11 22 99 88 99]) = map[A1:88 A2:22 A9:99], want = map[A1:88 A2:22 A9:99]
