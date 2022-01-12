36c64df746d7a9f2ac998cdb88e65e4c9fb64f72
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2022-01-12 15:23:03 +0200

    maps - main.go

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func createPhoneBook(names []string, numbers []string) map[string]string {
+	PhoneBook := make(map[string]string)
+	index := len(names)
+	for i := 0; i != index; i++ {
+		PhoneBook[names[i]] = numbers[i]
+	}
+	return PhoneBook
+}
+
+func main() {
+	var names, numbers []string
+	var i, size uint
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("How many contacts do you want to enter?")
+	fmt.Scan(&size)
+	for i = 0; i != size; i++ {
+		fmt.Print("Enter name #", i, " : ")
+		text1, _ := reader.ReadString('\n')
+		fmt.Print("Enter phone number #", i, " : ")
+		text2, _ := reader.ReadString('\n')
+		names = append(names, text1)
+		numbers = append(numbers, text2)
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCreatePhoneBook(t *testing.T) {
+	for _, tc := range []struct {
+		names   []string
+		numbers []string
+		want    map[string]string
+	}{
+		{[]string{"A1", "A2", "A3"}, []string{"+371222", "+371111", "+371333"}, map[string]string{"A1": "+371222", "A2": "+371111", "A3": "+371333"}},
+		{[]string{"+371234"}, []string{"A2"}, map[string]string{"+371234": "A2"}},
+		{[]string{"A1", "A1"}, []string{"2266", "+3389"}, map[string]string{"A2": "+3389"}},
+		{[]string{",!"}, []string{":0="}, map[string]string{",!": ":0="}},
+		{[]string{"A1", "A2", "A9", "A1", "A9"}, []string{"11", "22", "99", "88", "99"}, map[string]string{"A1": "88", "A2": "22", "A9": "99"}},
+	} {
+		got := createPhoneBook(tc.names, tc.numbers)
+		if reflect.DeepEqual(got, tc.want) {
+			t.Errorf("createPhoneBook(%v, %v) = %v, want = %v", tc.names, tc.numbers, got, tc.want)
+		} // Every Test fails, although everywhere got == want ??
+		//createPhoneBook([A1 A2 A9 A1 A9], [11 22 99 88 99]) = map[A1:88 A2:22 A9:99], want = map[A1:88 A2:22 A9:99]
+		//FAIL
+	}
+}
