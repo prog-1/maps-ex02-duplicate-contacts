5dd944d1a34da13dfe4ece2d6a9cbff1b8b35555
Vladimir Satinov <vovaumni@gmail.com>
2022-03-05 12:34:34 +0200

    Tests fixes

--- main_test.go
+++ main_test.go
@@ -1,37 +1,34 @@
 package main
 
-func TestCreatePhoneBook(t *testing.T){
-	for _, tc := []struct {
-		size uint
-		names []string
+import "testing"
+
+func TestCreatePhoneBook(t *testing.T) {
+	for _, tc := range []struct {
+		names   []string
 		numbers []string
-		want map[string]string
+		want    map[string]string
 	}{
 		{
-			0,
 			[]string{},
 			[]string{},
-			map[string]string{}
+			map[string]string{},
 		},
 		{
-			5,
 			[]string{"Alina", "Deniss B", "Antons", "Alina", "Antons"},
 			[]string{"+37126017505", "+37127785804", "+37123622588", "+37126505719", "+37128852154"},
 			map[string]string{"Alina": "+37126505719", "Antons": "+37128852154", "Deniss B": "+37127785804"},
 		},
 		{
-			4,
 			[]string{"Alina", "Alina", "Alina", "Alina"},
 			[]string{"+37126017505", "+371705", "+371234234237505", "+37126017505"},
 			map[string]string{"Alina": "+37126017505"},
 		},
 		{
-			3,
 			[]string{"Tihons", "Arina", "Egors"},
 			[]string{"+37134988264", "+37192744934", "+3712281337"},
 			map[string]string{"Tihons": "+37134988264", "Arina": "+37192744934", "Egors": "+3712281337"},
 		},
-	}{
+	} {
 		got := CreatePhoneBook(tc.names, tc.numbers)
 		if !equal(got, tc.want) {
 			t.Errorf("got = %q, want = %q", got, tc.want)
