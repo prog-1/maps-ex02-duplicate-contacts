778e3de52888297e6ac73aecb5257b2ce3097a94
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2022-05-04 12:40:01 +0300

    now here are right numbers

--- main.go
+++ main.go
@@ -21,10 +21,10 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("How many contacts do you want to enter?")
 	fmt.Scan(&size)
-	for i = 0; i != size; i++ {
+	for i = 1; i != size+1; i++ {
 		fmt.Print("Enter name #", i, " : ")
 		text1, _ := reader.ReadString('\n')
-		fmt.Print("Enter phone number #", i+1, " : ")
+		fmt.Print("Enter phone number #", i, " : ")
 		text2, _ := reader.ReadString('\n')
 		names = append(names, text1)
 		numbers = append(numbers, text2)
